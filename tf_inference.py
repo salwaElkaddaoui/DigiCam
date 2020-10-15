c36205cc6542abd8c79d832dc4c0dacd5b3fb7b8
salwa el kaddaoui <elkaddaoui.salwa@gmail.com>
2020-10-15 02:21:44 +0200

    replaced cv2 with matplotlib

--- tf_inference.py
+++ tf_inference.py
@@ -1,31 +1,33 @@
 import tensorflow as tf
 from tensorflow.python.platform import gfile
-import sys, cv2
+import matplotlib.image as mpimg
 import numpy as np
+import argparse
 
-def read_image(image_path):
-    image = tf.image.decode_jpeg( tf.io.read_file( image_path ) )
-    image = tf.expand_dims( tf.image.resize_images(image, (28, 28) ), axis=0 )
-    image = tf.math.multiply( tf.cast( image, tf.float32 ), 1/255. )
-    return image
+def rgb2gray(img):
+    R = img[:, :, 0]
+    G = img[:, :, 1]
+    B = img[:, :, 2]
+    img_gray = R * 299. / 1000 + G * 587. / 1000 + B * 114. / 1000
+    return img_gray
 
-image_path = 'images/6_inv_dil.jpg'
-# x = read_image(image_path)
-# x = tf.image.decode_jpeg( tf.io.read_file( image_path ) )
-x = np.array( [ np.expand_dims( cv2.resize( cv2.imread(image_path, 0), (28, 28) ), axis=2 ) ] ).astype(np.float32)/255.
+parser = argparse.ArgumentParser(description='Process some integers.')
+parser.add_argument('--im', dest='im', help='image path')
+parser.add_argument('--mo', dest='mo', help='path to frozen graph file')
+args = parser.parse_args()
+if not os.path.exists(args.im):
+    raise IOError('Given image path does not exist')
+if not os.path.exists(args.mo):
+    raise IOError('Given frozen graph file does not exist')
 
+
+x = np.array( [ np.expand_dims( rgb2gray(mpimg.imread(args.im)), axis=2 ) ] ).astype(np.float32)/255.
 with tf.Session() as sess:
-    with gfile.FastGFile('ckpt/best_model.pb', 'rb') as f:
+    with gfile.FastGFile(args.mo, 'rb') as f:
         graph_def = tf.GraphDef()
         graph_def.ParseFromString(f.read())
         sess.graph.as_default()
         g_in = tf.import_graph_def(graph_def)
-
-        print('\n\n\n\n')
-        print(sess.graph.get_operations())
-        print('\n\n\n\n')
-        for op in sess.graph.get_operations():
-            print(op)
         tensor_input = sess.graph.get_tensor_by_name('import/conv2d_input:0')
         tensor_output = sess.graph.get_tensor_by_name('import/dense/Softmax:0')
         predictions = sess.run(tensor_output, {tensor_input: x})
