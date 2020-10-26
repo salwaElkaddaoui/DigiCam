b2536b971c345e92d0cd49b4ec1eaea1935c42c9
salwa el kaddaoui <elkaddaoui.salwa@gmail.com>
2020-10-26 12:17:14 +0100

    refactoring code

--- keras_inference.py
+++ keras_inference.py
@@ -1,30 +1,63 @@
 """
 Inference on a keras model
 Path to input image and path to h5 file shoud be given as arguments to this script
+
+Author: Salwa eL KADDAOUI
 """
 
 import sys, os
-import tensorflow as tf
-import numpy as np
-import argparse
+import argparse, logging
 
-def read_image(image_path):
+import numpy as np
+import tensorflow as tf
+
+def read_image(image_path: str) -> tf.Tensor:
+    """Reads an image with tensoflow.
+        #Arguments:
+            image_path: path to the image to read
+        #Returns:
+            the image as a tensorflow tensor
+    """
     image = tf.image.decode_jpeg( tf.io.read_file( image_path ) )
     image = tf.expand_dims( tf.image.resize_images(image, (28, 28) ), axis=0 )
     image = tf.math.multiply( tf.cast( image, tf.float32 ), 1/255. )
     return image
 
-parser = argparse.ArgumentParser(description='Process some integers.')
-parser.add_argument('--im', dest='im', help='image path')
-parser.add_argument('--mo', dest='mo', help='model path')
-args = parser.parse_args()
+def parse_arguments() -> argparse.Namespace:
+    """Parse script input arguments
+        #Arguments:
+            None
+        #Returns:
+            object containing attributes (==arguments)
+    """
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--im', dest='im', help='image path')
+    parser.add_argument('--mo', dest='mo', help='model path')
+    args = parser.parse_args()
 
-if not os.path.exists(args.im):
-    raise IOError('Given image path does not exist')
-if not os.path.exists(args.mo):
-    raise IOError('Given model path does not exist')
+    if not os.path.exists(args.im):
+        raise IOError('Given image path does not exist')
+    if not os.path.exists(args.mo):
+        raise IOError('Given model path does not exist')
 
-model = tf.keras.models.load_model(args.mo, compile = True)
-image = read_image(args.im)
-predictions = model.predict(image, steps=1)
-print("We think it's a {} with a confidence of {:.2f}".format(np.argmax(predictions), max(predictions[0])))
+    logging.info('Input Arguments successfully parsed.')
+    return args
+
+def main(args):
+    """Main function of th Program:
+        1. Load model
+        2. Read image
+        3. Make prediction and print predicted class and confidence
+        #Arguments:
+            Script parsed arguments
+        #Returns:
+            None
+    """
+    model = tf.keras.models.load_model(args.mo, compile = True)
+    image = read_image(args.im)
+    predictions = model.predict(image, steps=1)
+    logging.info("We think it's a {} with a confidence of {:.2f}".format(np.argmax(predictions), max(predictions[0])))
+
+if __name__=='__main__':
+    args = parse_arguments()
+    main(args)
